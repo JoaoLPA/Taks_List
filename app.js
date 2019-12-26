27d8454e63dbe52ccb873d949346b55c73d2c749
Joao Alves <joaol.alves@gmail.com>
2019-12-26 14:45:51 -0300

    Add task working

--- app.js
+++ app.js
@@ -14,8 +14,26 @@ const loadEventListeners = () => {
 // Functions
 
 const addTask = (event) => {
+  if(taskInput.value === ''){
+    alert('Adda a task');
+  }else{
+  //create nem list item
+  const li = document.createElement('li');
+  const link = document.createElement('a');
+  li.className = 'collection-item';
+  li.appendChild(document.createTextNode(taskInput.value));
+  link.className = 'delete-item secondary-content';
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  link.href = '#';
+  li.appendChild(link);
+
+  //append the new li to the ul
+  taskList.appendChild(li);
+  }
+  
+  //clear input field
+  taskInput.value = '';
   event.preventDefault();
-  console.log('working');
 };
 
 // Call function that load all events lisnteners
