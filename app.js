637cfceae58be59d0d241401811a8378cdb3afc5
Joao Alves <joaol.alves@gmail.com>
2019-12-26 23:19:41 -0300

    store and recover from Local Storage working

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const clearBtn = document.querySelector('.clear-tasks');
 
 //Function with events
 const loadEventListeners = () => {
+  document.addEventListener('DOMContentLoaded', getTasks);
   form.addEventListener('submit', addTask);
   taskList.addEventListener('click', removeTask);
   clearBtn.addEventListener('click', clearTasks);
@@ -15,41 +16,69 @@ const loadEventListeners = () => {
 }
 
 // Functions
+
+//Create a new task
 const addTask = (event) => {
-  if(taskInput.value === ''){
+  if(taskInput.value === '') {
     alert('Adda a task');
-  }else{
+  } else {
   //create nem list item
-  const li = document.createElement('li');
-  const link = document.createElement('a');
-  li.className = 'collection-item';
-  li.appendChild(document.createTextNode(taskInput.value));
-  link.className = 'delete-item secondary-content';
-  link.innerHTML = '<i class="fa fa-remove"></i>';
-  link.href = '#';
-  li.appendChild(link);
+  let newItem = createNewListItem(taskInput.value);
 
   //append the new li to the ul
-  taskList.appendChild(li);
+  taskList.appendChild(newItem);
   }
+
+  storeTask(taskInput.value);
   
   //clear input field
   taskInput.value = '';
   event.preventDefault();
 }
 
+// stores tasks on Local Storage
+const storeTask = (task) => {
+  let tasks;
+  if(localStorage.getItem('tasks') === null){
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.push(task);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// recovers stored tasks
+const getTasks = () => {
+  let tasks;
+  if(localStorage.getItem('tasks') === null){
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach(function(task) {
+    let newItem = createNewListItem(task);
+    taskList.appendChild(newItem);
+  })
+}
+
+// remove a task from the list
 const removeTask = (event) => {
   if(event.target.parentElement.classList.contains('delete-item')){
     event.target.parentElement.parentElement.remove();
   }
 }
 
+//clear all tasks from the list
 const clearTasks = () => {
   while(taskList.firstChild){
     taskList.removeChild(taskList.firstChild);
   }
 }
 
+// filter the displayed tasks
 const filterTasks = (event) => {
   const text = event.target.value.toLowerCase();
   
@@ -63,9 +92,21 @@ const filterTasks = (event) => {
       }
     }
   )
-
 }
 
+//create li for the tasks list
+const createNewListItem = (task) => {
+  const li = document.createElement('li');
+  const link = document.createElement('a');
+  li.className = 'collection-item';
+  li.appendChild(document.createTextNode(task));
+  link.className = 'delete-item secondary-content';
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  link.href = '#';
+  li.appendChild(link);
+
+  return li;
+}
 // Call function that load all events lisnteners
 loadEventListeners();
 
